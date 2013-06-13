9b0fd9ffda6f0d41a10d490504e3aee556a4978a
slug <slug404@hotmail.com>
2013-06-13 12:21:00 +0800

    删除原先在这里定义的FileInfor,将其定义更改到Common中

--- TcpServer/RunnableBase.h
+++ TcpServer/RunnableBase.h
@@ -6,34 +6,7 @@
 #include <QMap>
 #include <QString>
 
-struct FileInfor
-{
-    bool operator < (const FileInfor &other) const
-    {
-        if(fileName < other.fileName)
-        {
-            return true;
-        }
-        else if(fileName == other.fileName)
-        {
-            if(filePath < other.filePath)
-            {
-               return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    QString fileName;
-    QString filePath;
-};
+#include "Common.h"
 
 class RunnableBase : public QObject, public QRunnable
 {
